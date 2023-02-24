8ec14f1cc4d44580b74f0ae55b45239b39c807b0
Bruno Ribaric <bruno@ribarich.me>
2023-02-24 17:53:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Duplicator script
+
+Script that runs Duplicator Pro through Puppeteer to clone a website into LocalWP.
+
+Documentation coming soon
---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+const { program } = require("commander");
+const duplicatorRecording = require("./lib/duplicator-recording");
+
+async function main() {
+  program.version("0.1.0");
+
+  program
+    .command("clone")
+    .argument("<upstreamUrl>", "Upstream URL")
+    .requiredOption("--downstream-url <http://example.local>", "Downstream URL")
+    .requiredOption(
+      "--local-site-name <example>",
+      "Name of the local site. Directory within Local Sites."
+    )
+    .requiredOption("--username <username>")
+    .requiredOption("--password <password>")
+    .option("--db-only", "Clone database only")
+    .action(async (upstreamUrl, options) => {
+      try {
+        await duplicatorRecording({ ...options, upstreamUrl });
+      } catch (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
+
+  await program.parseAsync();
+}
+
+main();
